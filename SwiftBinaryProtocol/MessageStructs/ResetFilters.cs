12e25c9c51307e2d544fc5c18830cee29a6f2657
mathias13 <mathias@nolgarden.net>
2015-07-12 22:19:22 +0200

    Fixed reference to ResetFilter enum

--- SwiftBinaryProtocol/MessageStructs/ResetFilters.cs
+++ SwiftBinaryProtocol/MessageStructs/ResetFilters.cs
@@ -14,9 +14,9 @@ namespace SwiftBinaryProtocol.MessageStructs
         public ResetFilters(byte[] data)
         {
             int filter = Convert.ToInt32(data[0]);
-            FilterEnum filterTypeEnum = ResetFilters.FilterEnum.UNKNOWN;
-            if (Enum.IsDefined(typeof(FilterEnum), (int)filter))
-                filterTypeEnum = (FilterEnum)(int)filter;
+            SBP_Enums.ResetFilter filterTypeEnum = SBP_Enums.ResetFilter.UNKNOWN;
+            if (Enum.IsDefined(typeof(SBP_Enums.ResetFilter), (int)filter))
+                filterTypeEnum = (SBP_Enums.ResetFilter)(int)filter;
             _filter = filterTypeEnum;
         }
 
