87f8b19a8c9991237d99ad7b9b0b8881c4082e41
mathias13 <mathias@nolgarden.net>
2015-08-04 11:50:49 +0200

    Fixed all message ids

--- SwiftBinaryProtocol/SBP_Enums.cs
+++ SwiftBinaryProtocol/SBP_Enums.cs
@@ -8,14 +8,22 @@ namespace SwiftBinaryProtocol
         {
             Unknown = 0x0000,
             PRINT = 0x0010,
+            TWEET = 0x0012,
+            ACQ_RESULT = 0x0014,
+            TRACKING_STATE = 0x0013,
             THREAD_STATE =  0x0017,
             UART_STATE = 0x0018,
             IAR_STATE = 0x0019,
+            MASK_SATELLITE = 0x001B,
             RESET_FILTERS = 0x0022,
             INIT_BASE = 0x0023,
-            OBS_HDR = 0x0043,
+            OBS = 0x0041,
             BASEPOS = 0x0044,
+            OBS_HDR = 0x0045,
+            EPHEMERIS = 0x0047,
+            RESET = 0x00B2,
             GPSTIME = 0x0100,
+            EXT_EVENT = 0x0101,
             POS_ECEF = 0x0200,
             POS_LLH = 0x0201,
             BASELINE_ECEF = 0x0202,
@@ -32,6 +40,27 @@ namespace SwiftBinaryProtocol
             SinglePointPosition = 0,
             Fixed_RTK = 1,
             Float_RTK = 2
+        }
+
+        public enum SatteliteMask
+        {
+            MASK_ACQUISITION = 1,
+            MASK_TRACKING = 2,
+            UNKNOWN = -1
+        }
+
+        public enum ResetFilter
+        {
+            IAR = 1,
+            DGNSS = 0,
+            UNKNOWN = -1
+        }
+
+        public enum TrackingState
+        {
+            DISABLED = 0,
+            ENABLED = 1,
+            UNKNOWN = -1
         }
     }
 }
